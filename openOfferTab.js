96d4ebe3fc6a25d0f0127b44b46dd4d0e0ec6d64
vhchung <vhchung@users.noreply.github.com>
2016-06-28 23:00:24 +0700

    Update openOfferTab.js
    Remove require jquery because it crash outerHTML function

--- openOfferTab.js
+++ openOfferTab.js
@@ -5,15 +5,15 @@
 // @description  try to take over the world!
 // @author       You
 // @match        http://localhost:8080/financial-product/personal-loan
-// @require      https://code.jquery.com/jquery-2.2.4.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var offerTab = $('a[href="#products"][role="tab"]');
+    var offerTab = document.querySelector('a[href="#products"][role="tab"]');
     console.log('offerTab: ', offerTab);
-    if (offerTab.length > 0)
-    	offerTab.css('pointer-events', 'auto');
+    if (offerTab.length > 0) {
+    	offerTab.style.pointerEvents = "all";
+    }
     // Your code here...
 })();
